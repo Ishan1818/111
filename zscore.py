3852ed729f857641a536497dacbcf317d45d2710
Ishan1818 <72986416+Ishan1818@users.noreply.github.com>
2021-12-24 20:59:28 +0530

    Add files via upload

---
+++ zscore.py
@@ -0,0 +1,99 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+
+df = pd.read_csv("data1.csv")
+data = df["Math_score"].tolist()
+
+
+
+#calculating the mean and standard deviation of the population data
+mean = statistics.mean(data)
+std_deviation = statistics.stdev(data)
+print("mean of  population:- ",mean)
+print("Standard deviation of population:- ", std_deviation)
+
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+
+print("std1",first_std_deviation_start, first_std_deviation_end )
+print("std2",second_std_deviation_start, second_std_deviation_end )
+print("std3",third_std_deviation_start, third_std_deviation_end )
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+
+
+# Pass the number of time you want the mean of the data points as a parameter in range function in for loop
+mean_list = []
+for i in range(0,1000):
+    set_of_means= random_set_of_mean(100)
+    mean_list.append(set_of_means)
+
+std_deviation_sample = statistics.stdev(mean_list)
+mean_sample = statistics.mean(mean_list)
+print("mean of sampling distribution:- ",mean_sample)
+print("Standard deviation of sampling distribution:- ", std_deviation_sample)
+
+
+# # finding the mean of THE STUDENTS WHO GAVE EXTRA TIME TO MATH LAB and plotting on graph
+df = pd.read_csv("data1.csv")
+data = df["Math_score"].tolist()
+mean_of_sample1 = statistics.mean(data)
+print("Mean of sample1:- ",mean_of_sample1)
+fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[mean_of_sample1, mean_of_sample1], y=[0, 0.17], mode="lines", name="MEAN OF STUDENTS WHO HAD MATH LABS"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+# #finding the mean of the STUDENTS WHO USED MATH PRACTISE APP and plotting it on the plot.
+
+df = pd.read_csv("data2.csv")
+data = df["Math_score"].tolist()
+mean_of_sample2 = statistics.mean(data)
+print("mean of sample 2:- ",mean_of_sample2)
+fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[mean_of_sample2, mean_of_sample2], y=[0, 0.17], mode="lines", name="MEAN OF STUDENTS WHO USED THE APP"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+
+
+# finding the mean of the STUDENTS WHO WERE ENFORCED WITH REGISTERS and plotting it on the plot.
+df = pd.read_csv("data3.csv")
+data = df["Math_score"].tolist()
+mean_of_sample3 = statistics.mean(data)
+print("mean of sample3:- ",mean_of_sample3)
+fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[mean_of_sample3, mean_of_sample3], y=[0, 0.17], mode="lines", name="MEAN OF STUDNETS WHO WERE ENFORCED WITH MATH REGISTERS"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+z_score=(mean_of_sample1-mean)/std_deviation
+print("the zscore is=", z_score)
+z_score1=(mean_of_sample2-mean)/std_deviation
+print("the zscore is=", z_score1)
+z_score2=(mean_of_sample3-mean)/std_deviation
+print("the zscore is=", z_score2)
+
+
